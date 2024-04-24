f8160ccce2e5c6dffbe5a4f750c9accaecc1fde7
Dmytro <xisgame113@gmail.com>
2024-04-24 09:55:56 +0200

    first commit

---
+++ src/components/Clear/Clear.jsx
@@ -0,0 +1,22 @@
+import './Clear.css'
+
+function Clear() {
+    return (
+        <div className='clear'>
+            <div className="text">
+                <div className="image-clear">
+                    <img src="https://static1.s123-cdn-static-a.com/ready_uploads/media/7029141/800_5f9ccc00dd131.jpg" alt=""></img>
+                </div>
+                <span className="info">
+                    <h3>INTERNET RZECZY (IOT) [68 GODZIN]</h3>
+                    <span>Jeśli żywisz fascynację wobec technologii i jesteś gotowy/a na eksplorację świata IoT, ta ścieżka jest jak najbardziej dla Ciebie. Dedykowana jest raczej, ale nie tylko, osobom o umyśle ścisłym, pragnącym zgłębić tajniki elektrotechniki, systemów wbudowanych, oraz wnikliwie poznać język C++. Czy marzysz o stworzeniu własnego robota? A może planujesz wyposażyć swój dom w inteligentne rozwiązania sterowane za pomocą smartfona? Ta ścieżka nie tylko zaspokoi Twoją ciekawość, ale dostarczy wiedzy umożliwiającej zrealizowanie tych marzeń.</span>
+                    <button className='button-right'>UCZ SIĘ 7 DNI ZA DARMO</button>
+                </span>
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default Clear;
---
+++ src/components/Main/Main.js
@@ -0,0 +1,20 @@
+import './Main.css';
+
+function Main() {
+    return (
+        <div className="main">
+            <div className="text">
+                <span className="info">
+                    <h3>INTERNET RZECZY (IOT) [68 GODZIN]</h3>
+                    <span>Jeśli żywisz fascynację wobec technologii i jesteś gotowy/a na eksplorację świata IoT, ta ścieżka jest jak najbardziej dla Ciebie. Dedykowana jest raczej, ale nie tylko, osobom o umyśle ścisłym, pragnącym zgłębić tajniki elektrotechniki, systemów wbudowanych, oraz wnikliwie poznać język C++. Czy marzysz o stworzeniu własnego robota? A może planujesz wyposażyć swój dom w inteligentne rozwiązania sterowane za pomocą smartfona? Ta ścieżka nie tylko zaspokoi Twoją ciekawość, ale dostarczy wiedzy umożliwiającej zrealizowanie tych marzeń.</span>
+                </span>
+                <div className="image">
+                    <img src="https://static1.s123-cdn-static-a.com/ready_uploads/media/7029141/800_5f9ccc00dd131.jpg" alt=""></img>
+                </div>
+            </div>
+            <button>UCZ SIĘ 7 DNI ZA DARMO</button>
+        </div>
+    );
+}
+
+export default Main;
